426b3dca8b6fa3417bb954e2883ac91d968c1cea
Michael Babker <michael.babker@gmail.com>
2018-10-17 22:23:45 -0500

    Test debug command

---
+++ Tests/Command/DebugRouterCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Gos\Bundle\PubSubRouterBundle\Tests\Command;
+
+use Gos\Bundle\PubSubRouterBundle\Command\DebugRouterCommand;
+use Gos\Bundle\PubSubRouterBundle\Loader\YamlFileLoader;
+use Gos\Bundle\PubSubRouterBundle\Router\Router;
+use Gos\Bundle\PubSubRouterBundle\Router\RouterRegistry;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class DebugRouterCommandTest extends TestCase
+{
+    public function testCommandListsRoutesForARouter()
+    {
+        $command = new DebugRouterCommand($this->buildRegistryWithValidRouter());
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            [
+                '--router_name' => 'test',
+            ]
+        );
+
+        $this->assertStringEqualsFile(__DIR__.'/../Fixtures/command_output/valid_router.txt', $commandTester->getDisplay());
+    }
+
+    public function testCommandRaisesErrorIfRouterDoesNotExist()
+    {
+        $command = new DebugRouterCommand($this->buildRegistryWithValidRouter());
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            [
+                '--router_name' => 'missing',
+            ]
+        );
+
+        $this->assertStringEqualsFile(__DIR__.'/../Fixtures/command_output/missing_router.txt', $commandTester->getDisplay());
+        $this->assertSame(1, $commandTester->getStatusCode());
+    }
+
+    private function buildRegistryWithValidRouter()
+    {
+        $router = new Router(
+            'test',
+            new YamlFileLoader(new FileLocator([__DIR__.'/../Fixtures'])),
+            [
+                'validchannel.yml',
+            ]
+        );
+
+        $registry = new RouterRegistry();
+        $registry->addRouter($router);
+
+        return $registry;
+    }
+}
