68712c3a8dc0dbb1c9688c3ffc400123921a1823
Michael Babker <michael.babker@gmail.com>
2019-01-11 14:06:01 -0600

    Update config for Symfony 4.2 change

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('gos_pubsub_router');
+        $treeBuilder = new TreeBuilder('gos_pubsub_router');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('gos_pubsub_router');
 
         $rootNode
             ->addDefaultsIfNotSet()
