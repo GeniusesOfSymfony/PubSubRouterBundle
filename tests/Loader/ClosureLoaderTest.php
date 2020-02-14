f8071c2609d4fa41b25742e9b0743ad08320bb34
Michael Babker <michael.babker@gmail.com>
2020-02-14 15:29:45 -0600

    Add support for using Closures in loading routes

---
+++ tests/Loader/ClosureLoaderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Gos\Bundle\PubSubRouterBundle\Tests\Loader;
+
+use Gos\Bundle\PubSubRouterBundle\Loader\ClosureLoader;
+use Gos\Bundle\PubSubRouterBundle\Router\Route;
+use Gos\Bundle\PubSubRouterBundle\Router\RouteCollection;
+use PHPUnit\Framework\TestCase;
+
+final class ClosureLoaderTest extends TestCase
+{
+    public function testSupports()
+    {
+        $loader = new ClosureLoader();
+
+        $closure = function () {};
+
+        $this->assertTrue($loader->supports($closure), '->supports() returns true if the resource is loadable');
+        $this->assertFalse($loader->supports('foo.foo'), '->supports() returns true if the resource is loadable');
+
+        $this->assertTrue($loader->supports($closure, 'closure'), '->supports() checks the resource type if specified');
+        $this->assertFalse($loader->supports($closure, 'foo'), '->supports() checks the resource type if specified');
+    }
+
+    public function testLoad()
+    {
+        $loader = new ClosureLoader();
+
+        $route = new Route('/', 'strlen');
+
+        $routes = $loader->load(function () use ($route) {
+            $routes = new RouteCollection();
+
+            $routes->add('foo', $route);
+
+            return $routes;
+        });
+
+        $this->assertEquals($route, $routes->get('foo'), '->load() loads a \Closure resource');
+    }
+}
