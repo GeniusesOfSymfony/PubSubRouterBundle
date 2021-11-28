f4c0a82447f31532c8a3ce33592ad92d048699dc
Michael Babker <michael.babker@gmail.com>
2021-11-28 13:29:36 -0600

    Deprecate the getters for readonly properties in the PubSubRequest

--- src/Request/PubSubRequest.php
+++ src/Request/PubSubRequest.php
@@ -9,23 +9,41 @@ final class PubSubRequest
 {
     public readonly ParameterBag $attributes;
 
-    public function __construct(public readonly string $routeName, public readonly Route $route, array $attributes)
-    {
+    public function __construct(
+        public readonly string $routeName,
+        public readonly Route $route,
+        array $attributes
+    ) {
         $this->attributes = new ParameterBag($attributes);
     }
 
+    /**
+     * @deprecated to be removed in 4.0, read the route name from the `$routeName` property instead
+     */
     public function getRouteName(): string
     {
+        trigger_deprecation('gos/pubsub-router-bundle', '3.0', 'The %s() method is deprecated and will be removed in 4.0. Read the route name from the $routeName property instead.', __METHOD__);
+
         return $this->routeName;
     }
 
+    /**
+     * @deprecated to be removed in 4.0, fetch the route from the `$route` property instead
+     */
     public function getRoute(): Route
     {
+        trigger_deprecation('gos/pubsub-router-bundle', '3.0', 'The %s() method is deprecated and will be removed in 4.0. Fetch the route from the $route property instead.', __METHOD__);
+
         return $this->route;
     }
 
+    /**
+     * @deprecated to be removed in 4.0, read the attributes from the `$attributes` property instead
+     */
     public function getAttributes(): ParameterBag
     {
+        trigger_deprecation('gos/pubsub-router-bundle', '3.0', 'The %s() method is deprecated and will be removed in 4.0. Read the attributes from the $attributes property instead.', __METHOD__);
+
         return $this->attributes;
     }
 }
